6d19920c71f402ed49561cad0692324b38984d22
emanoj1 <manojventures@gmail.com>
2023-12-11 17:26:16 +1100

    Start Checklist functions file

---
+++ checklist_functions.py
@@ -0,0 +1,12 @@
+def add_checklist():
+    print("Add checklist")
+
+def view_checklist():
+    print("View item")
+
+def remove_checklist():
+    print("Remove item")
+
+def mark_checklist():
+    print("Mark item")
+
--- travel-checklist.py
+++ travel-checklist.py
@@ -1,3 +1,5 @@
+from checklist_functions import add_checklist, view_checklist, remove_checklist, mark_checklist
+
 #Welcome message
 print("\nWelcome to your Travel Checklist!")
 print("Keep a track of what to take and don't miss a thing!!")
@@ -8,7 +10,7 @@ def create_menu_list():
     print("\n1. Add item - Enter 1")
     print("2. View item - Enter 2")
     print("3. Remove item - Enter 3")
-    print("4. View full list - Enter 4")
+    print("4. Mark item - Enter 4")
     print("5. Exit - Enter 5")
     menu_choice = input("\nEnter your selection: ") #menu_choice is a local variable
     return(menu_choice)
@@ -20,13 +22,13 @@ user_choice_menu = ""
 while user_choice_menu != "5":
     user_choice_menu = create_menu_list()
     if (user_choice_menu == "1"):
-        print("Your input is 1")
+        add_checklist()
     elif (user_choice_menu == "2"):
-        print("Your input 2")
+        view_checklist()
     elif (user_choice_menu == "3"):
-        print("Your input 3")
+        remove_checklist()
     elif (user_choice_menu == "4"):
-        print("Your input 4")
+        mark_checklist()
     elif (user_choice_menu == "5"):
         continue # will skip next line and continue with the loop
     else:
