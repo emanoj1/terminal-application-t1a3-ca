4e86c7a41e7d7000e07cc7d47ac5976bc3a567fc
emanoj1 <manojventures@gmail.com>
2023-12-16 11:54:47 +1100

    Create error handling,CSV

--- checklist_functions.py
+++ checklist_functions.py
@@ -1,12 +1,24 @@
-def add_checklist():
-    print("Add checklist")
+import csv
 
-def view_checklist():
+def add_checklist(file_name):
+    print("Add checklist")
+    # Ask title for the Add Checklist option
+    checklist_name = input("Enter the item to carry: ")
+    
+    # Open the CSV file
+    with open(file_name, "a") as f:
+        writer = csv.writer(f)
+    
+    # Insert values - titl = user entered
+                    # - Completed = False
+        writer.writerow([checklist_name, "False"])
+
+def view_checklist(file_name):
     print("View item")
 
-def remove_checklist():
+def remove_checklist(file_name):
     print("Remove item")
 
-def mark_checklist():
+def mark_checklist(file_name):
     print("Mark item")
 
--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from checklist_functions import add_checklist, view_checklist, remove_checklist, mark_checklist
 
+file_name = "travel-checklist.csv"
+
+try:
+    # Open file in Read mode
+    todo_file = open(file_name, "r")
+    todo_file.close()
+    print("In try block")
+    # if error, file doesn't exist (go to Except block)
+    # if no error, file exists
+except FileNotFoundError:
+    # Since file doesn't exist, create the file and insert the first line into the file
+    todo_file = open(file_name, "w")
+    todo_file.write("title,completed\n")
+    todo_file.close()
+    print("In except block")
+
 #Welcome message
 print("\nWelcome to your Travel Checklist!")
 print("Keep a track of what to take and don't miss a thing!!")
@@ -22,16 +38,17 @@ user_choice_menu = ""
 while user_choice_menu != "5":
     user_choice_menu = create_menu_list()
     if (user_choice_menu == "1"):
-        add_checklist()
+        add_checklist(file_name)
     elif (user_choice_menu == "2"):
-        view_checklist()
+        view_checklist(file_name)
     elif (user_choice_menu == "3"):
-        remove_checklist()
+        remove_checklist(file_name)
     elif (user_choice_menu == "4"):
-        mark_checklist()
+        mark_checklist(file_name)
     elif (user_choice_menu == "5"):
         continue # will skip next line and continue with the loop
     else:
         print("\nInvalid input - Choose a number between 1-5 only.")
 
 print("Thank you for using Travel Checklist! Bon Voyage!")
+print("\n")
