5faf73335d01bf427bb6925c40c9f79f72b0384a
emanoj1 <manojventures@gmail.com>
2023-12-23 13:35:34 +1100

    Code cleanup in Remove option

--- checklist_functions.py
+++ checklist_functions.py
@@ -26,7 +26,7 @@ def view_checklist(file_name):
 
 def remove_checklist(file_name):
     # Ask title for the REMOVE Checklist option
-    checklist_name = input("Remove the item you don't need anymore: ")
+    checklist_name = input("Enter the item name you don't need anymore: ")
     # Copy into a new CSV file after checking if its in the original list, else don't copy
     checklist_items = []
     with open(file_name, "r") as f:
@@ -38,6 +38,7 @@ def remove_checklist(file_name):
         writer = csv.writer(f)
         writer.writerows(checklist_items)
     print("\nDone! That item was removed!")
+
 
 def mark_checklist(file_name):
     checklist_name = input("Which item do you want to mark as packed?: ")
@@ -53,4 +54,3 @@ def mark_checklist(file_name):
         writer  = csv.writer(f)
         writer.writerows(checklist_items)
     print("\nDone! Your chosen item is packed!")
-
