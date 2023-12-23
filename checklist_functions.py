1ce52fcd1d30e2e0d0ff426f902e1dbaa71590f8
emanoj1 <manojventures@gmail.com>
2023-12-23 16:34:38 +1100

    Check for item not found in Remove option

--- checklist_functions.py
+++ checklist_functions.py
@@ -26,20 +26,26 @@ def view_checklist(file_name):
                 print(f">>> {row[0]} : Yet to pack.") # display message if NOT packed
 
 def remove_checklist(file_name):
-    # Ask title for the REMOVE Checklist option
-    checklist_name = input("Enter the item name you don't need anymore: ")
-    # Copy into a new CSV file after checking if its in the original list, else don't copy
-    checklist_items = []
-    with open(file_name, "r") as f:
-        reader = csv.reader(f)
-        for row in reader:
-            if (checklist_name != row[0]):
-                checklist_items.append(row)
-    with open(file_name, "w") as f:
-        writer = csv.writer(f)
-        writer.writerows(checklist_items)
-    print("\nDone! That item was removed!")
+    checklist_name = input("Enter the item name you don't need anymore: ") # Ask title for the REMOVE Checklist option
+    checklist_items = [] # An empty list to store items from the CSV file
+    item_found = False # To keep track of entered items, and see if it exists in the list or not
+    
+    with open(file_name, "r") as f: # Open CSV file in read mode
+        reader = csv.reader(f) # create a reader for the CSV
+        for row in reader: # iterate through each row in the file
+            if (checklist_name != row[0]): # If input item doesn't match the first item in the list
+                checklist_items.append(row) # then append the row into checklist_items 
+            else:
+                item_found = True # else if it finds a match, set to item_found to True
 
+    with open(file_name, "w") as f: # Open the CSV file
+        writer = csv.writer(f) # create a CSV writer
+        writer.writerows(checklist_items) # write the updated list by removing the item if it was found
+
+    if item_found:
+        print("\nDone! That item was removed!") # When the value is True, it confirms item removal
+    else:
+        print("\nSorry! Item not in current list. Please view your list and enter the correct item name") #When the value is False, it alerts the user of the absent item
 
 def mark_checklist(file_name):
     checklist_name = input("Which item do you want to mark as packed?: ")
